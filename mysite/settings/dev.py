d7efce8e1ca1403d6bd1eb05be55e3ed941dc6eb
JM Acera <john.acera@sportality.se>
2024-03-28 11:09:30 +0800

    initial commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-_7nr!9l7n5g@x&_$(0lz(9(gpsj@^(y+(dg&1d1#hqr*9qz&d_"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
